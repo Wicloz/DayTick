45e3bdbd7e1a2b475372b7f66b2ba635f0c5ef86
Wilco de Boer <deboer.wilco@gmail.com>
2025-06-15 01:31:04 +0200

    Install Materialize CSS and use SASS for compilation

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,7 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import './index.scss'
 import App from './App.tsx'
+import 'materialize-css/dist/js/materialize.min.js'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
